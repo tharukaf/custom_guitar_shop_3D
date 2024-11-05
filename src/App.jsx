4f7cb2763f4d1010b843881ff38f6efe5b1b22ab
Nipun Tharuka Fernando <nipunjayanga@gmail.com>
2024-11-05 12:09:48 -0500

    material change achieved

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ height: '100%' }}>
       <Canvas>
         <Suspense fallback={null}>
+          <ambientLight intensity={1} />
+          <directionalLight position={[0, 0, 5]} rotateZ={0} intensity={0.9} />
+          <color attach="background" args={['white']} />
           <Model />
-          <OrbitControls />
-          <Environment preset="sunset" background />
+          <OrbitControls minDistance={3} maxDistance={7}/>
+          {/* <Environment preset="sunset" background /> */}
         </Suspense>
       </Canvas>
     </div>
@@ -22,8 +25,22 @@ export default function App() {
 
 const Model = () => {
   const gltf = useLoader(GLTFLoader, "./guitar.gltf");
+  gltf.scene.traverse((child) => {
+    if (child.isMesh) {
+      // id: 24 is the body of the guitar
+      if(child.id === 24){
+        child.material.color.set('#00ff00');
+      }
+      console.log(child.id ,child.material.color)
+      // child.material.color.set('#00ff00'); // Change to desired color
+    }
+  });
+  // console.log(gltf);
+
+  // gltf.scene.materials['000_1']
   return (
     <>
+      <OrbitControls />
       <primitive object={gltf.scene} scale={0.006} />
     </>
   );
