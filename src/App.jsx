45aa189299581fd3e7e1016b3b5db4c6d109ebfd
Nipun Tharuka Fernando <nipunjayanga@gmail.com>
2024-11-05 11:33:11 -0500

    initialized project

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Suspense } from "react";
+import "./App.css";
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <Canvas>
+        <Suspense fallback={null}>
+          <Model />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, "./guitar.gltf");
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.006} />
+    </>
+  );
+};
